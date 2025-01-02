759eacf6bc9a63141fb989b480acdca008f0631f
junior <shahid68491@gmail.com>
2025-01-02 13:21:53 +0500

    adding new file

---
+++ ExpoRouting/app/index.tsx
@@ -0,0 +1,21 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text
+        style={{
+          fontSize: 30,
+          color: "#323245",
+          backgroundColor:"#daaddd"
+        }}
+      >Hello world! this is shahid ali</Text>
+    </View>
+  );
+}
