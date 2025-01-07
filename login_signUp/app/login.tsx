93c3024ef601fb7662bccd5966d77992a38b3448
junior <shahid68491@gmail.com>
2025-01-07 23:37:04 +0500

    adding new files

--- login_signUp/app/login.tsx
+++ login_signUp/app/login.tsx
@@ -1,7 +1,9 @@
-import { View, Text, ImageBackground, Image, TextInput } from "react-native";
+import { View, Text, ImageBackground, Image, TextInput,StyleSheet, TouchableOpacity } from "react-native";
 import React from "react";
-import { useRouter } from "expo-router";
+import { Link, useRouter } from "expo-router";
 import MyButton from "@/components/myButton";
+import { FontAwesome } from "@expo/vector-icons";
+// import Svg, { Path } from "react-native-svg";
 const Login = () => {
   
   const router = useRouter();
@@ -26,31 +28,63 @@ const Login = () => {
       <View
         style={{
           padding: 20,
-          marginHorizontal:20,
+          marginHorizontal: 20,
           backgroundColor: "white",
         }}
       >
-        <TextInput placeholder="Enter Your Email"
-          style={{
-            height:50,
-            borderRadius: 10,
-            marginBottom: 10,
-            padding: 10,
-          }}
-        onChange={(e) => console.log(e.nativeEvent.text)}
+        <TextInput
+          placeholder="Enter Your Email"
+          style={styles.input}
+          onChange={(e) => console.log(e.nativeEvent.text)}
         />
-        <TextInput placeholder="Enter Your Password"
+        <TextInput
+          placeholder="Enter Your Password"
           secureTextEntry={true}
-          style={{
-            height:50,
-            borderRadius: 10,
-            marginBottom: 10,
-            padding:10
-          }}
+          style={styles.input}
         />
-      <MyButton title={"Login"} onPress={onlogin} />
+        <MyButton title={"Login"} onPress={onlogin} />
       </View>
+      <View style={styles.icons}>
+        <Text>
+          Don't Have any Account?{" "}
+          <Link href={"./signup"} style={{ color: "blue" }}>
+            SignUp
+          </Link>
+        </Text>
+      </View>
+      <View style={styles.icon}>
+        <TouchableOpacity>
+          <FontAwesome name="google" size={30} color="#DB4437" />
+          </TouchableOpacity> 
+       <TouchableOpacity>
+          <FontAwesome name="facebook" size={30} color="#316FF6" />
+          </TouchableOpacity>
+        </View>  
     </View>
-  );
-};
+  )
+}
 export default Login;
+
+
+
+const styles = StyleSheet.create({
+  input: {
+    height: 50,
+    borderRadius: 10,
+    marginBottom: 10,
+    padding: 10,
+    borderWidth: 0.1,
+    borderColor: "gray",
+  },
+  icons:{
+    flex: 1,
+    alignItems: "center",
+  },
+  icon:{
+    flex: 3,
+    flexDirection: "row",
+    justifyContent: "center",
+    gap:20
+  },
+  },
+)
